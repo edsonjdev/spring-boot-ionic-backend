0cf7a29ab3bab3dfec9cd37382ba72babe03edba
Edson J <edsonj.dev@gmail.com>
2019-06-13 20:29:27 +0200

    Ajustes no endpoint /categoria/{id}

--- src/main/java/com/edson/cursomc/services/CategoriaService.java
+++ src/main/java/com/edson/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.edson.cursomc.domain.Categoria;
 import com.edson.cursomc.repositories.CategoriaRepository;
+import com.edson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
