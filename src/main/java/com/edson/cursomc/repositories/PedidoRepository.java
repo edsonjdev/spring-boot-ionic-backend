dd07c75d89080344b524cd6d6956e04d4237f47f
Edson J <edsonj.dev@gmail.com>
2019-07-02 22:04:15 +0200

    Restrição de conteúdo: cliente so recupera seus pedidos

--- src/main/java/com/edson/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/edson/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.edson.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.edson.cursomc.domain.Cliente;
 import com.edson.cursomc.domain.Pedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, PageRequest pageRequest);
 
 }
--- src/main/java/com/edson/cursomc/services/PedidoService.java
+++ src/main/java/com/edson/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.edson.cursomc.domain.Cliente;
 import com.edson.cursomc.domain.ItemPedido;
 import com.edson.cursomc.domain.PagamentoComBoleto;
 import com.edson.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.edson.cursomc.domain.enums.EstadoPagamento;
 import com.edson.cursomc.repositories.ItemPedidoRepository;
 import com.edson.cursomc.repositories.PagamentoRepository;
 import com.edson.cursomc.repositories.PedidoRepository;
+import com.edson.cursomc.security.UserSS;
+import com.edson.cursomc.services.exceptions.AuthorizationException;
 import com.edson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,6 +76,17 @@ public class PedidoService {
 		emailService.sendOrderConfirmationEmail(obj);
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repository.findByCliente(cliente, pageRequest);
 	}
 
 }
