ab829589c53df992054026b7b774682d98711ffe
Edson J <edsonj.dev@gmail.com>
2019-07-02 20:40:17 +0200

    Implementando Autorizacao

--- src/main/java/com/edson/cursomc/config/SecurityConfig.java
+++ src/main/java/com/edson/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import com.edson.cursomc.security.JWTAuthenticationFilter;
+import com.edson.cursomc.security.JWTAuthorizationFilter;
 import com.edson.cursomc.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
