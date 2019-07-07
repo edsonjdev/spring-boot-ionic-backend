0ebfaa3c2b0f5ce64c695f80408570aa9b380028
Edson J <edsonj.dev@gmail.com>
2019-07-07 18:31:19 +0200

    Enviando imagem via endpoint

--- src/main/java/com/edson/cursomc/config/SecurityConfig.java
+++ src/main/java/com/edson/cursomc/config/SecurityConfig.java
@@ -44,11 +44,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
-			"/clientes/**"
+			"/clientes"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes/**",
+			"/clientes",
+			"/clientes/picture",
 			"/auth/forgot/**"
 	};
 	
