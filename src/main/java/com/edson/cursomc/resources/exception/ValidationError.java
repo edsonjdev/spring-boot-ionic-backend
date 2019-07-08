e4e8c4df7413d9e68e48aca9cd229447091d53e7
Edson J <edsonj.dev@gmail.com>
2019-07-08 22:07:55 +0200

    Padronizando formato das exceções

--- src/main/java/com/edson/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/edson/cursomc/resources/exception/ValidationError.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ValidationError extends StandardError {
+	
 	private static final long serialVersionUID = 1L;
 	
 	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
+	}
 
 	public List<FieldMessage> getErrors() {
 		return errors;
@@ -16,8 +21,5 @@ public class ValidationError extends StandardError {
 		errors.add(new FieldMessage(fieldName, messagem));
 	}
 
-	public ValidationError(Integer status, String msg, long timeStamp) {
-		super(status, msg, timeStamp);
-	}
 
 }
